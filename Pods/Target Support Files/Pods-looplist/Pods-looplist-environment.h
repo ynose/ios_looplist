6b8a0b7b27c8bf60dc7aec316ce7a8754cdd6ace
Yoshio Nose <ynose.249@gmail.com>
2015-06-21 14:43:51 +0900

    NendをCocoaPodsに変更

---
+++ Pods/Target Support Files/Pods-looplist/Pods-looplist-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// NendSDK_iOS
+#define COCOAPODS_POD_AVAILABLE_NendSDK_iOS
+#define COCOAPODS_VERSION_MAJOR_NendSDK_iOS 2
+#define COCOAPODS_VERSION_MINOR_NendSDK_iOS 7
+#define COCOAPODS_VERSION_PATCH_NendSDK_iOS 0
+
